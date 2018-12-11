57c43c9aeb8ebbfcbe5e121bb9641e3ea5d861be
yuanlingxiang <ylx330328@163.com>
2018-12-11 22:39:41 +0800

    第二章习题：13题
    Signed-off-by: yuanlingxiang <ylx330328@163.com>

---
+++ Charpter2/2-13.py
@@ -0,0 +1,11 @@
+#! usr/bin/env/python
+# -*- coding:utf-8 -*-
+
+import sys
+
+# 执行 dir(sys), 你就可以看到sys模块的所有属性了
+print (dir(sys))
+# 显示 sys 模块的版本号属性及平台变量
+print (sys.version)
+# 调用sys.exit()函数, 退出Python 解释器的方式
+sys.exit(0)
